2e34ffd21fafbdedd588763710718852364f12ed
Cristian <cristiancarrasco80@gmail.com>
2019-10-09 13:07:33 -0300

    Paginacion

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("h2").css('font-weight', 'bold');
+  $("h2").css('font-size', '3rem');
+
+  $("h3").css('font-weight', 'bold');
+  $("h3").css('font-size', '1.5rem');
+
+
+
+  $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+  })
+});
